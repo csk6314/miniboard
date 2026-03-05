f2b50c3c46cbc4a71e57027e2d5b88bc659b6a45
csk6314 <37896060+csk6314@users.noreply.github.com>
2026-03-06 01:17:23 +0900

    feat: create Comment Entity

---
+++ src/main/java/com/miniboard/backend/comment/domain/Comment.java
@@ -0,0 +1,43 @@
+package com.miniboard.backend.comment.domain;
+
+import com.miniboard.backend.domain.BaseEntity;
+import com.miniboard.backend.member.domain.UserEntity;
+import com.miniboard.backend.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="comments")
+@Getter
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="author_id", nullable = false)
+    private UserEntity author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="parent_id")
+    private Comment parent;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean isDelete;
+
+    public Comment(Post post, UserEntity author, Comment parent, String content) {
+        this.post = post;
+        this.author = author;
+        this.parent = parent;
+        this.content = content;
+    }
+}
