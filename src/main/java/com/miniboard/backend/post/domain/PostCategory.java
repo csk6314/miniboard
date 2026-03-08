0048ac342b93460d4f65865cc2950a3c433cd5d8
csk6314 <37896060+csk6314@users.noreply.github.com>
2026-03-09 02:38:04 +0900

    feat: add Unique Constraints on PostCategory

--- src/main/java/com/miniboard/backend/post/domain/PostCategory.java
+++ src/main/java/com/miniboard/backend/post/domain/PostCategory.java
@@ -8,7 +8,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Table(name="post_category")
+@Table(name = "post_category",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_post_category",
+                columnNames = {"post_id", "category_id"}
+        )
+)
 public class PostCategory {
 
     @Id
@@ -16,11 +21,11 @@ public class PostCategory {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="post_id",nullable = false)
+    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="category_id",nullable = false)
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
 
