d7803d2d552481c01ae1f48c61c717b58f0fbab5
채승규 <cotmdrb@gmail.com>
2026-03-19 17:57:07 +0900

    feat: add Categories in PostService

--- src/main/java/com/miniboard/backend/post/service/PostService.java
+++ src/main/java/com/miniboard/backend/post/service/PostService.java
@@ -2,7 +2,12 @@ package com.miniboard.backend.post.service;
 
 import com.miniboard.backend.member.domain.UserEntity;
 import com.miniboard.backend.member.repository.UserRepository;
+import com.miniboard.backend.post.domain.Category;
 import com.miniboard.backend.post.domain.Post;
+import com.miniboard.backend.post.domain.PostCategory;
+import com.miniboard.backend.post.dto.PostDetailResponseDto;
+import com.miniboard.backend.post.repository.CategoryRepository;
+import com.miniboard.backend.post.repository.PostCategoryRepository;
 import com.miniboard.backend.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +22,11 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
+    private final CategoryRepository categoryRepository;
+    private final PostCategoryRepository postCategoryRepository;
+
     @Transactional
-    public Long createPost(Long userId, String title, String content) {
+    public Long createPost(Long userId, String title, String content, List<Long> categoryIds) {
         UserEntity user =  userRepository.findById(userId)
                 .orElseThrow(()->new IllegalArgumentException("유저를 찾을 수 없습니다."));
 
@@ -26,12 +34,36 @@ public class PostService {
 
         postRepository.save(post);
 
+        addCategories(userId, categoryIds, post);
+
         return post.getId();
     }
 
-    public Post getPost(Long postId) {
-        return postRepository.findById(postId)
+    private void addCategories(Long userId, List<Long> categoryIds, Post post) {
+        for(Long categoryId : categoryIds) {
+            Category category = categoryRepository.findById(categoryId)
+                    .orElseThrow(()->new IllegalArgumentException("카테고리를 찾을 수 없습니다."));
+
+            validateCategoryOwner(userId, category);
+
+            PostCategory postCategory = new PostCategory(post, category);
+            postCategoryRepository.save(postCategory);
+        }
+    }
+
+    private void validateCategoryOwner(Long userId, Category category) {
+        if(!category.getUser().getId().equals(userId)) {
+            throw new IllegalArgumentException("본인이 만든 카테고리만 수정할 수 있습니다");
+        }
+    }
+
+    public PostDetailResponseDto getPostDetail(Long postId) {
+        Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+
+        List<PostCategory> categories = postCategoryRepository.findByPostId(postId);
+
+        return PostDetailResponseDto.from(post, categories);
     }
 
     public List<Post> getPosts() {
@@ -50,6 +82,8 @@ public class PostService {
 
         post.updateTitle(title);
         post.updateContent(content);
+
+
     }
 
     @Transactional
